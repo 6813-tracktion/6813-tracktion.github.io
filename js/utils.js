3c5c7a27ba308263932671fcccbbe5742bd287e2
Matt McCutchen <rmccutch@mit.edu>
2015-04-15 14:55:47 -0400

    Fix week ordering (was dependent on object iteration order).

--- js/utils.js
+++ js/utils.js
@@ -7,5 +7,5 @@ function dayFormat(m) {
 function weekFormat(m) {
     // TODO: Let the user change the current goal cycle without affecting
     // past goal periods.  Can probably wait for GR5.
-    return moment(m).format('GGGG-[W]W'); // e.g. 2015-W7
+    return moment(m).format('GGGG-[W]WW'); // e.g. 2015-W7
 };
