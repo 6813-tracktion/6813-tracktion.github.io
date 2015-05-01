67b7a873df91d169aa1fce619ea453a403f17cf1
Matt McCutchen <rmccutch@mit.edu>
2015-04-30 21:53:45 -0400

    Axes and ticks for the long-range view.
    Also make the vertical axes opaque black 2px-wide for consistency.

--- js/longRangeView.js
+++ js/longRangeView.js
@@ -5,10 +5,19 @@ var LongRangeView = Marionette.ItemView.extend({
         this.listenTo(this.model.attributes.weeks, 'add change:goal change:total remove reset', this.throttledRender);
     },
     templateHelpers: function() {
+        // Do this computation only once per render.
+        var maxMinutes = Math.max(420,
+                _.max(this.model.attributes.weeks.pluck('total')),
+                _.max(this.model.attributes.weeks.pluck('goal')));
+        var PX_PER_MIN = (198 /*total usable width*/ / maxMinutes);
+        var maxTicks = 5;
+        // Choose an integer number of hours at which we can draw a tick.
+        var hoursPerTick = Math.floor(maxMinutes / 60 / (maxTicks+1)) + 1;
         return {
             WEEK_HEIGHT_PX: 13,
-            // Do this computation only once per render.
-            PX_PER_MIN: (200 /*total width*/ / Math.max(420, _.max(this.model.attributes.weeks.pluck('total')))),
+            PX_PER_MIN: PX_PER_MIN,
+            maxMinutes: maxMinutes,
+            hoursPerTick: hoursPerTick,
             // Round so our 1px borders are aligned with the screen pixels.
             rm2p: function(min) {
                 return Math.round(this.PX_PER_MIN * min);
--- tmpl/longRangeView.tpl
+++ tmpl/longRangeView.tpl
@@ -1,5 +1,12 @@
 <script type="text/template" id="longRangeViewTpl">
-  <svg width="200" height="<%= weeks.length * WEEK_HEIGHT_PX %>">
+  <svg width="200" height="<%= 21 + weeks.length * WEEK_HEIGHT_PX %>"><g transform="translate(1,21)">
+  <% for (var i = 1; 60*i*hoursPerTick <= maxMinutes; i++) { %>
+    <line x1="<%= rm2p(60*i*hoursPerTick) %>" x2="<%= rm2p(60*i*hoursPerTick) %>" y1="-7" y2="-2" stroke="black" stroke-width="2px"/>
+    <text style="text-anchor: middle;" x="<%= rm2p(60*i*hoursPerTick) %>" y="-10"><%= i*hoursPerTick %>h</text>
+  <% } %>
+  <!-- Leave 2px vertical margin before the first week row so the goal mark
+       doesn't join the axis. -->
+  <line x1="-1" x2="199" y1="-2" y2="-2" stroke="black" stroke-width="2px"/>
   <% weeks.each(function(week, i){ %>
   <g transform="translate(0, <%= i * WEEK_HEIGHT_PX + 1 %>)" class="longRangeWeek" data-cid="<%= week.cid %>">
     <!-- So that hovering anywhere in the row counts as a hover on the g.day
@@ -11,5 +18,6 @@
      stroke="black" stroke-width="2"/>
   </g>
   <% }) %>
-  </svg>
+  <line class="separator" x1="0" x2="0" y1="-7" y2="<%= weeks.length * WEEK_HEIGHT_PX %>"/>
+  </g></svg>
 </script>
